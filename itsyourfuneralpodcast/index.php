d367dbd2061d556820905cc0b777c9250e2bfa76
Kelley Kavanaugh <kelleykavanaugh@gmail.com>
2013-09-12 14:14:31 -0700

    changed title to dynamic wordpress title

--- itsyourfuneralpodcast/index.php
+++ itsyourfuneralpodcast/index.php
@@ -2,8 +2,8 @@
 <div class="container">
   <!-- Main hero unit for a primary marketing message or call to action -->
   <div class="hero-unit">
-    <h1>Hello, world!</h1>
-    <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+    <h1>It's Your Funeral!</h1>
+    <p>This is where Scout will put the mission statement things</p>
     <p><a href="#" class="btn btn-primary btn-large">Learn more &raquo;</a></p>
   </div><!-- End Hero Unit -->
   <div id="content">
