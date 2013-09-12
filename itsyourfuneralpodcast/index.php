2d8060a8ac9547ff59d54fc10af831392f5e45c4
Kelley Kavanaugh <kelleykavanaugh@gmail.com>
2013-09-12 13:59:03 -0700

    cleaned up index to be more generic

--- itsyourfuneralpodcast/index.php
+++ itsyourfuneralpodcast/index.php
@@ -1,14 +1,10 @@
 <?php get_header(); ?>
-<h2><center>Friday, <?php echo get_the_date(); ?></h2></center>
   <div id="content">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?><!-- Start The Loop -->
-      <div class="span4">
-      <div id="category">
-        <?php $category = get_the_category(); if($category[0]){echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>'; } ?>
-      </div><!-- End Category-->
-      <center><h4><a href="<?php the_permalink() ?>" rel="bookmark" title="Read <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h4></center>
+      <div class="span12">
+      <center><h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Read <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1></center>
       <p class="text-right"><small><em>By: <?php the_author_posts_link() ?></em></small></p>
-      <?php the_excerpt() ?>
+      <?php the_content() ?>
     </div><!-- End Span 4-->
     <?php endwhile; else: ?>
     <p>Sorry, no posts matched your criteria.</p>
@@ -16,8 +12,8 @@
   </div><!-- End "content" -->
   <div class="span12">
     <!--Start Nav-->
-    <?php previous_posts_link( 'Newer Issue' ); ?>//
-    <?php next_posts_link( 'Older Issue' , $max_pages ); ?>
+    <?php previous_posts_link( 'Newer Episodes' ); ?>//
+    <?php next_posts_link( 'Older Episodes' , $max_pages ); ?>
     <!--End Nav-->
   </div><!-- End 12 span -->
 <?php get_footer(); ?>
