2d12678ea5f92d2ce958a082b353b2a1c448aba5
Kelley Kavanaugh <kelleykavanaugh@gmail.com>
2013-09-17 10:55:25 -0700

    adding bottom-border with padding and margin

--- itsyourfuneralpodcast/index.php
+++ itsyourfuneralpodcast/index.php
@@ -5,10 +5,11 @@
       <!-- Start The Loop -->
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
           <center><h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Read <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1></center>
-          <?php the_time('F jS, Y') ?>
-          <p class="text-right">Post By: <?php the_author_posts_link() ?></p>
+          <p class="pull-left"><?php the_time('F jS, Y') ?></p>
+          <p class="pull-right">Post By: <?php the_author_posts_link() ?></p>
+        <div class="pull-left border-bottom">
           <?php the_content() ?>
-
+        </div>
           <?php endwhile; else: ?>
           <p>Sorry, no posts matched your criteria.</p>
           <?php endif; ?>
