a29f6ae298300a806116d83f669622e9080d9ca5
Kelley Kavanaugh <kelleykavanaugh@gmail.com>
2013-09-17 19:32:38 -0700

    finally reached a color scheme I like

--- itsyourfuneralpodcast/functions.php
+++ itsyourfuneralpodcast/functions.php
@@ -14,7 +14,7 @@ $args = array(
 	'width'         => 1170,
 	'flex-height'    => true,
 	'height'        => 358,
-	'default-image' => get_template_directory_uri() . '/images/header.jpg',
+	'default-image' => get_template_directory_uri() . '/images/header.png',
 );
 add_theme_support( 'custom-header', $args );
 
